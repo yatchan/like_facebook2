3daa0664695822f90de43655ff227d6fcc6ba228
yatchan <jazzguitar.tokyo@gmail.com>
2017-10-14 12:38:17 +0000

    facebook用に変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/yatchan/like_facebook2'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/facebook'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
